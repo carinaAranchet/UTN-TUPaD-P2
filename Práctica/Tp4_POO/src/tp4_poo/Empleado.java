6e8c7507f7360854bff49c1e1ff0d491400ec737
carinaAranchet <81835193+carinaAranchet@users.noreply.github.com>
2025-11-21 11:01:09 -0300

    entrega tp 4

---
+++ Práctica/Tp4_POO/src/tp4_poo/Empleado.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp4_poo;
+
+/**
+ *
+ * @author Carina Aranchet
+ */
+public class Empleado {
+    private int id; 
+    private String nombre; 
+    private String puesto; 
+    private double salario; 
+    private static int totalEmpleados; 
+    
+    //constructor 
+    public Empleado(int id, String nombre, String puesto, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+        totalEmpleados++;
+    }
+    
+    //constructor reducido
+    public Empleado(String nombre, String puesto) {
+        totalEmpleados++;
+        this.id = totalEmpleados; //id se asigna automaticamente por el contador
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = 0.0;
+    }
+    
+    
+    public void actualizarSalario(double porcentaje){
+        this.salario += this.salario * (porcentaje / 100.0);
+    }
+    
+    
+    public void actualizarSalario(int montoFijo){
+        this.salario += montoFijo;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Empleado {" + "id = " + id + ", nombre = " 
+                + nombre + ", puesto = " + puesto + ", salario = " + salario + '}';
+        
+    }
+    
+    
+    public static int mostrarTotalEmpleados() {
+        return totalEmpleados;
+    }
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+    
